494523a7d36fa990c11591b3a05d7139f45ae955
Aashish-kushwaha <kushwahaashu444@gmail.com>
2022-09-04 15:41:05 +0530

    completed face_detection

--- face_detection.py
+++ face_detection.py
@@ -4,14 +4,16 @@ import cv2
 face=cv2.CascadeClassifier("D:\projects/face_detection/haarcascade_frontalface_default.xml")
 
 #reading the image
-img=cv2.imread("D:\projects/face_detection/face.jpeg",0)
+img=cv2.imread("D:\projects/face_detection/face.jpeg",1)
+#converting image into grayscale image
+gray_img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-
-faces=face.detectMultiScale(img,1.05,3)
+#search the co-ordinates of the image
+faces=face.detectMultiScale(gray_img,1.05,3)
 
 for( x,y,w,h) in faces:
      img=cv2.rectangle(img, (x,y),(x+w,y+h),(0,233,0),3)
-
+#resizing the image
 rimg=cv2.resize(img,(int(img.shape[1]/2),int(img.shape[0]/2)))
 
 cv2.imshow('new',rimg)
